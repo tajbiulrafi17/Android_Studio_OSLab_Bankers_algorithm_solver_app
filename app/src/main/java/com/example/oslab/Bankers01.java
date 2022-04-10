70c6e011f1a14e7aa25c896c41638010bd1685fd
Tajbiul Hassan Rafi <tajbiul.rafi@northsouth.edu>
2022-04-11 03:58:23 +0600

    Welcome User Interface

---
+++ app/src/main/java/com/example/oslab/Bankers01.java
@@ -0,0 +1,14 @@
+package com.example.oslab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bankers01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bankers01);
+    }
+}
